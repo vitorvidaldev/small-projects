f3bfd1ecbd3862ccdb01bd646a5c1ac485764dd8
Vitor Vidal <vitorvidal.dev@gmail.com>
2023-04-11 08:26:26 -0300

    testing kafka concurrency

--- Kafka/udemy-course/consumer/src/main/kotlin/dev/vitorvidal/udemykafkaconsumer/udemykafka/consumer/LibraryEventsConsumer.kt
+++ Kafka/udemy-course/consumer/src/main/kotlin/dev/vitorvidal/udemykafkaconsumer/udemykafka/consumer/LibraryEventsConsumer.kt
@@ -1,11 +1,16 @@
 package dev.vitorvidal.udemykafkaconsumer.udemykafka.consumer
 
 import lombok.extern.slf4j.Slf4j
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
+import java.lang.management.ManagementFactory
+import java.lang.management.ThreadInfo
+import java.lang.management.ThreadMXBean
+
 
 @Slf4j
 @Component
@@ -14,9 +19,11 @@ class LibraryEventsConsumer {
         private val log: Logger = LoggerFactory.getLogger(this::class.java)
     }
 
-    @KafkaListener(topics = ["library-events"])
-    fun onMessage(consumerRecord: List<Any>, acknowledgment: Acknowledgment?) {
-        log.info("ConsumerRecord: {}", consumerRecord)
-        acknowledgment?.acknowledge()
+    @KafkaListener(topics = ["library-events"], concurrency = "100")
+    fun onMessage(consumerRecord: List<ConsumerRecord<String, Any>>, acknowledgment: Acknowledgment?) {
+        consumerRecord.forEach {
+            log.info("ConsumerRecord: {}", it)
+            acknowledgment?.acknowledge()
+        }
     }
 }
