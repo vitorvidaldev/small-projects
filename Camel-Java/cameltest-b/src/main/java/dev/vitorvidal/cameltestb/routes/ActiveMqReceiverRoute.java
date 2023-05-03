935b88f51f3ce509c6e200cc8c5fe1a2d8c1bca6
Vitor Vidal <vitorvidal.dev@gmail.com>
2023-05-03 19:26:42 -0300

    adding new project for udemy course

--- Camel-Java/cameltest-b/src/main/java/dev/vitorvidal/cameltestb/routes/ActiveMqReceiverRoute.java
+++ Camel-Java/cameltest-b/src/main/java/dev/vitorvidal/cameltestb/routes/ActiveMqReceiverRoute.java
@@ -2,6 +2,7 @@ package dev.vitorvidal.cameltestb.routes;
 
 import dev.vitorvidal.cameltestb.component.MyCurrencyExchangeProcessor;
 import dev.vitorvidal.cameltestb.model.CurrencyExchange;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,12 @@ public class ActiveMqReceiverRoute extends RouteBuilder {
     @Autowired
     MyCurrencyExchangeProcessor myCurrencyExchangeProcessor = new MyCurrencyExchangeProcessor();
 
+    @Autowired
+    ProducerTemplate producerTemplate;
+
     @Override
     public void configure() throws Exception {
+
         from("activemq:my-activemq-queue")
                 .unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
                 .bean(myCurrencyExchangeProcessor)
---
+++ Spring-Boot/petclinic/src/test/java/dev/vitorvidal/petclinic/PetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vitorvidal.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
