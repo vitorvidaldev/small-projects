7706267e341bdac8841689585957b99275083adb
Vitor Vidal <vitorvidal.dev@gmail.com>
2023-05-29 21:14:10 -0300

    adding dependency injection test project

---
+++ Spring-Boot/dependencyinjection/src/main/java/dev/vitorvidal/dependencyinjection/DependencyinjectionApplication.java
@@ -0,0 +1,13 @@
+package dev.vitorvidal.dependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyinjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyinjectionApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot/dependencyinjection/src/test/java/dev/vitorvidal/dependencyinjection/DependencyinjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vitorvidal.dependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyinjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
