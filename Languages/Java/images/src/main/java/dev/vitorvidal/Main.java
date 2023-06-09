d63a83c77ce426db50851e69d2e81ffecee4e175
Vitor Vidal <vitorvidal.dev@gmail.com>
2023-06-09 20:02:26 -0300

    Updating project to use opencv library

--- Languages/Java/images/src/main/java/dev/vitorvidal/Main.java
+++ Languages/Java/images/src/main/java/dev/vitorvidal/Main.java
@@ -1,38 +1,43 @@
 package dev.vitorvidal;
 
-import nu.pattern.OpenCV;
-import org.opencv.core.*;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgproc.Imgproc;
 
 public class Main {
     public static void main(String[] args) {
-        OpenCV.loadShared();
+        OpenCVUtils.initializeOpenCV();
 
         Mat image = ImageOpener.openImage("./src/main/resources/images/beau.png");
-        Mat kernel = new Mat();
-        Mat dst = new Mat();
+        String windowName = "Sobel Demo - Simple Edge Detector";
+        Mat grayscaleImage = new Mat();
+        Mat xGray = new Mat();
+        Mat yGray = new Mat();
+        Mat xGrayAbs = new Mat();
+        Mat yGrayAbs = new Mat();
+        Mat gradient = new Mat();
+        int scale = 2;
+        int delta = 0;
+        int depth = CvType.CV_16S;
 
-        Point anchor = new Point(-1, -1);
-        double delta = 0.0;
-        Integer depth = -1;
-        int kernelSize;
+        // Remove noise by blurring with a Gaussian filter (kernel size = 3)
+        Imgproc.GaussianBlur(image, image, new Size(3, 3), 0, 0, Core.BORDER_DEFAULT);
 
-        int ind = 0;
-        while (true) {
-            kernelSize = 3 + 2 * (ind % 5);
-            Mat ones = Mat.ones(kernelSize, kernelSize, CvType.CV_32F);
-            Core.multiply(ones, new Scalar(1 / (double) (kernelSize * kernelSize)), kernel);
+        // Convert the image to grayscale
+        Imgproc.cvtColor(image, grayscaleImage, Imgproc.COLOR_RGB2GRAY);
 
-            Imgproc.filter2D(image, dst, depth, kernel, anchor, delta, Core.BORDER_DEFAULT);
-            HighGui.imshow("window", dst);
-            int c = HighGui.waitKey(500);
+        Imgproc.Sobel(grayscaleImage, xGray, depth, 1, 0, 3, scale, delta, Core.BORDER_DEFAULT);
+        Imgproc.Sobel(grayscaleImage, yGray, depth, 0, 1, 3, scale, delta, Core.BORDER_DEFAULT);
 
-            if (c == 27) {
-                break;
-            }
-            ind++;
-        }
-        System.exit(0);
+        Core.convertScaleAbs(xGray, xGrayAbs);
+        Core.convertScaleAbs(yGray, yGrayAbs);
+
+        Core.addWeighted(xGrayAbs, 0.5, yGrayAbs, 0.5, 0, gradient);
+
+        HighGui.imshow(windowName, gradient);
+        HighGui.waitKey(0);
     }
 }
